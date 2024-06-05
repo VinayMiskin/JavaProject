7c7e14c8ed92bc3f25d554fddce76cacaa5320e9
V <V@DESKTOP-3GBSK4I>
2024-06-05 18:07:45 +0530

    Student Obj created with StudentManager as controller

---
+++ Studentmanager/src/com/vinay/Test/StudentManager/StudentManager.java
@@ -0,0 +1,16 @@
+package com.vinay.Test.StudentManager;
+
+import java.util.List;
+
+public class StudentManager {
+	
+	List<Student> studentList;
+	String path;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
